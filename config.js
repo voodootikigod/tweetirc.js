981719180f921d079bb3cbf7295e86a955e96479
Chris Williams <chris@iterativedesigns.com>
2010-05-22 11:57:00 -0400

    First post.

---
+++ config.js
@@ -0,0 +1,14 @@
+exports.load = (function() {
+	return {
+		twitter: {
+			account: "twitter",
+			password: "twitter",
+			terms: ['goruco']
+		},
+		irc: {
+			nick: "grctweets",
+			room: "#nyc.rb",
+			server: "irc.freenode.net"
+		}
+	};
+});
---
+++ tweetirc.js
@@ -0,0 +1,40 @@
+var TwitterNode = require('./twitter-node/lib').TwitterNode;
+var IRC        = require("./IRC/lib/irc");
+var config      = require("./config").load()
+var sys         = require('sys');
+
+var bot = new IRC({ 
+	server: config.irc.server, 
+	nick: config.irc.nick, 
+	channels: [config.irc.room] 
+})
+
+bot.connect(function(){
+    setTimeout(function() {
+      // Join channels
+      if (!!bot.options.channels && bot.options.channels instanceof Array)
+        for (var i = 0; i < bot.options.channels.length; i++) {
+          this.join(bot.options.channels[i])
+					this.privmsg(bot.options.channels[i], "I am tweetcasting for your mom!")
+				}
+    }.bind(this), 15000)
+  })
+
+
+var twit = new TwitterNode({ 
+	user: config.twitter.account, 
+	password: config.twitter.password,  
+	track: config.twitter.terms	
+});
+
+
+twit
+  .addListener('tweet', function(tweet) {
+		bot.privmsg(room, "@" + tweet.user.screen_name + ": " + tweet.text)
+  }).addListener('limit', function(limit) {
+    sys.puts("LIMIT: " + sys.inspect(limit));
+  }).addListener('delete', function(del) {
+    sys.puts("DELETE: " + sys.inspect(del));
+  }).addListener('end', function(resp) {
+    sys.puts("wave goodbye... " + resp.statusCode);
+  }).stream();
