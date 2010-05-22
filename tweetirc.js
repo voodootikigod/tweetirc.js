7118386e23471f825dd984ae0c012f9f955a8448
Chris Williams <chris@iterativedesigns.com>
2010-05-22 12:12:50 -0400

    Update for the crazy commafirst format.

--- tweetirc.js
+++ tweetirc.js
@@ -3,11 +3,10 @@ var IRC        = require("./IRC/lib/irc");
 var config      = require("./config").load()
 var sys         = require('sys');
 
-var bot = new IRC({ 
-	server: config.irc.server, 
-	nick: config.irc.nick, 
-	channels: [config.irc.room] 
-})
+var bot = new IRC({ server: config.irc.server
+                    , nick: config.irc.nick
+                    , channels: [config.irc.room] 
+                  })
 
 bot.connect(function(){
     setTimeout(function() {
@@ -15,22 +14,21 @@ bot.connect(function(){
       if (!!bot.options.channels && bot.options.channels instanceof Array)
         for (var i = 0; i < bot.options.channels.length; i++) {
           this.join(bot.options.channels[i])
-					this.privmsg(bot.options.channels[i], "I am tweetcasting for your mom!")
-				}
+          this.privmsg(bot.options.channels[i], "I am tweetcasting for your mom!")
+        }
     }.bind(this), 15000)
   })
 
 
-var twit = new TwitterNode({ 
-	user: config.twitter.account, 
-	password: config.twitter.password,  
-	track: config.twitter.terms	
-});
+var twit = new TwitterNode({  user: config.twitter.account
+                              , password: config.twitter.password
+                              , track: config.twitter.terms 
+                          });
 
 
 twit
   .addListener('tweet', function(tweet) {
-		bot.privmsg(config.irc.room, "@" + tweet.user.screen_name + ": " + tweet.text)
+    bot.privmsg(config.irc.room, "@" + tweet.user.screen_name + ": " + tweet.text)
   }).addListener('limit', function(limit) {
     sys.puts("LIMIT: " + sys.inspect(limit));
   }).addListener('delete', function(del) {
